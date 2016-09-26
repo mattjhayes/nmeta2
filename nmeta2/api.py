a9e8f45cfab0c6bd90c0c5d3b3dc8c1dacea7dfc
mattjhayes <matthew_john_hayes@hotmail.com>
2016-09-26 21:50:28 +1300

    Version compatibility check on DPAE only warns if major version number differs

--- nmeta2/api.py
+++ nmeta2/api.py
@@ -463,7 +463,8 @@ class RESTAPIController(ControllerBase):
             return ({'status': 400, 'msg': dpae_req_body.error})
 
         #*** Check version compatibility:
-        if dpae_req_body['dpae_version'] != nmeta.version:
+        if not version_compare(dpae_req_body['dpae_version'],
+                                                                nmeta.version):
             self.logger.warning("Possible version compatibility issue. "
                         "DPAE_version=%s nmeta2_version=%s",
                         dpae_req_body['dpae_version'], nmeta.version)
@@ -707,6 +708,18 @@ class JSON_Body(object):
             return self.req_body[key]
         else:
             return 0
+
+def version_compare(version1, version2):
+    """
+    Compare two semantic version numbers and return 1 if they
+    are the same major version number
+    """
+    (major1, minor1, patch1) = version1.split('.')
+    (major2, minor2, patch2) = version1.split('.')
+    if major1 == major2:
+        return 1
+    else:
+        return 0
 
 class Api(object):
     """
